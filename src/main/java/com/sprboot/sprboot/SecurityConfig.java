ded39f5b4a08593472bdb8d324323dc81e716f88
kahweng <ngkahweng2003@gmail.com>
2025-11-02 14:24:15 +0800

    Deployment CORS fix

--- src/main/java/com/sprboot/sprboot/SecurityConfig.java
+++ src/main/java/com/sprboot/sprboot/SecurityConfig.java
@@ -36,7 +36,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // add , "https://myfrontend.com"
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://securetrack-frontend.vercel.app")); // add
+                                                                                                                      // ,
+                                                                                                                      // "https://myfrontend.com"
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Content-Disposition"));
